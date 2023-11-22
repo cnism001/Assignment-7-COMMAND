04a3e01b47aa5a70e39dbf7406273f4adcbd91b5
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-22 17:47:02 +0200

    added Light and Thermostat classes for receivers and methods for them

--- Receivers.cs
+++ Receivers.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace Assignment_7_COMMAND
 {
-    internal class Receivers
+    // Light class for IoT device turning light on or off
+    public class Light
     {
+        // Method to turn  the light on
+        public void On()
+        {
+            Console.WriteLine("Light is turned ON");
+        }
+
+        // Method to turn  the light off
+        public void Off()
+        {
+            Console.WriteLine("Light is turned OFF");
+        }
     }
+
+    // Thermostat class representing IoT device that can increase or decrease termostat temperature
+    public class Thermostat
+    {
+        // Method to increase the temperature
+        public void IncreaseTemperature()
+        {
+            Console.WriteLine("Temperature increased");
+        }
+
+        // Method to decrease the temperature
+        public void DecreaseTemperature()
+        {
+            Console.WriteLine("Temperature decreased");
+        }
+    }
+
 }
