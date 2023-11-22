ccb5468371dcdb9bce70f2532851216bdea053bc
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-22 17:58:13 +0200

    added RemoteController class and methods

--- RemoteController.cs
+++ RemoteController.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Assignment_7_COMMAND
 {
-    internal class RemoteController
+    // RemoteController class acts is an invoker for command pattern
+    public class RemoteController
     {
+        private ICommand[] commands; // Array to store command objects
+
+        // Constructor initializing the command array
+        public RemoteController()
+        {   
+            // Assuming 10 buttons, error handling added later if any buttons are not assigned
+            commands = new ICommand[10]; 
+                                        
+        }
+
+        // Method to assign a command to a button
+        public void SetCommand(int button, ICommand command)
+        {
+            commands[button] = command;
+        }
+
+        // Method to press a button, executing the command assigned to the button
+        public void PressButton(int button)
+        {
+            //if value for a button exists in the array, so its been assigned
+            if (commands[button] != null)
+            {
+                commands[button].Execute();
+            }
+            else
+            {
+                Console.WriteLine("No command assigned to this button");
+            }
+        }
     }
+
 }
