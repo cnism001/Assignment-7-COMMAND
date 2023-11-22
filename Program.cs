8ec4dd1c235e27615998e6747bc61200d07d6ed0
Nikita Smirnov <cnism001@edu.xamk.fi>
2023-11-22 18:22:19 +0200

    added instances to main program, created composite command nightMode

--- Program.cs
+++ Program.cs
@@ -10,6 +10,25 @@ namespace Assignment_7_COMMAND
     {
         static void Main(string[] args)
         {
+            // Creating instances of receivers
+            Light light = new Light();
+            Thermostat thermostat = new Thermostat();
+
+            // Creating instance of the invoker
+            RemoteController remote = new RemoteController();
+
+            // Setting up commands for invoker
+            remote.SetCommand(1, new TurnOnLightCommand(light));
+            remote.SetCommand(2, new TurnOffLightCommand(light));
+            remote.SetCommand(3, new IncreaseTemperatureCommand(thermostat));
+            remote.SetCommand(4, new DecreaseTemperatureCommand(thermostat));
+
+            // Composite command example (Turn off light and decrease temperature)
+            CompositeCommand nightMode = new CompositeCommand();
+            nightMode.Add(new TurnOffLightCommand(light));
+            nightMode.Add(new DecreaseTemperatureCommand(thermostat));
+            remote.SetCommand(5, nightMode);
+
         }
     }
 }
